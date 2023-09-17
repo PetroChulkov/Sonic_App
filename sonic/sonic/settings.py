9e03e5bc712b49a164e1999732c6577f1f0b2211
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-09-17 18:43:27 +0200

    failcommit

--- sonic/sonic/settings.py
+++ sonic/sonic/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "media/"
