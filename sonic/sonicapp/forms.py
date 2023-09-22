ed159968165ce332ff14ea5208fb97a0397953b7
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-09-22 21:26:25 +0200

    edited login html

--- sonic/sonicapp/forms.py
+++ sonic/sonicapp/forms.py
@@ -41,7 +41,16 @@ class RegisterUserForm(UserCreationForm):
 
 
 class LoginUserForm(AuthenticationForm):
-    username = CharField(label="Login", widget=TextInput(attrs={"class": "form-input"}))
+    tailwind_class = """
+        px-5
+        block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 
+        ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
+        focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6
+        """
+
+    username = CharField(
+        label="Login", widget=TextInput(attrs={"class": tailwind_class})
+    )
     password = CharField(
-        label="Password", widget=PasswordInput(attrs={"class": "form-input"})
+        label="Password", widget=PasswordInput(attrs={"class": tailwind_class})
     )
