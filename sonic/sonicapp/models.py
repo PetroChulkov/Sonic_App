3208cdce930490485cb023ae30063ca797128eee
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-09-22 15:21:40 +0200

    users books and requests history

--- sonic/sonicapp/models.py
+++ sonic/sonicapp/models.py
@@ -1,15 +1,19 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
 
-# Create your models here.
 class PDFile(models.Model):
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to="uploads/")
-    # text = models.TextField(default="")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("pdfile_detail", args=[str(self.id)])
 
 
 class Question(models.Model):
