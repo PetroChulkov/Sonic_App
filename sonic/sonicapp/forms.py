f18e38234d07fe6f2e3f3576366c4103ac41abfc
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-09-22 21:54:08 +0200

    fixed login html form

--- sonic/sonicapp/forms.py
+++ sonic/sonicapp/forms.py
@@ -25,14 +25,25 @@ class PDFileForm(forms.ModelForm):
 
 
 class RegisterUserForm(UserCreationForm):
-    username = CharField(label="Login", widget=TextInput(attrs={"class": "form-input"}))
-    email = EmailField(label="Email", widget=EmailInput(attrs={"class": "form-input"}))
+    tailwind_class = """
+        px-5
+        block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 
+        ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
+        focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6
+        """
+
+    username = CharField(
+        label="Login", widget=TextInput(attrs={"class": tailwind_class})
+    )
+    email = EmailField(
+        label="Email", widget=EmailInput(attrs={"class": tailwind_class})
+    )
     password1 = CharField(
-        label="Password", widget=PasswordInput(attrs={"class": "form-input"})
+        label="Password", widget=PasswordInput(attrs={"class": tailwind_class})
     )
     password2 = CharField(
         label="Password confirmation",
-        widget=PasswordInput(attrs={"class": "form-input"}),
+        widget=PasswordInput(attrs={"class": tailwind_class}),
     )
 
     class Meta:
