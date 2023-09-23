74e04e79250d851c75d820c9fbf3ca801b7272dc
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-09-23 21:41:22 +0200

    changed bg color

--- sonic/sonicapp/forms.py
+++ sonic/sonicapp/forms.py
@@ -21,7 +21,7 @@ class PDFileForm(forms.ModelForm):
     class Meta:
         tailwind_class = """
         px-5
-        block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 
+        block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 
         ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
         focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6
         """
