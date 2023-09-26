c79db49c1684c317eac7ddd1d28ae5069773df25
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-09-26 19:12:10 +0200

    edited settings.py

--- sonic/sonic/settings.py
+++ sonic/sonic/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", env("MY_IP")]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
